bd941ca5d8090cbec711e52810121ddc35339657
Christopher Durham <cad97@cad97.com>
2024-04-13 14:49:20 -0400

    style improvements

--- src/macros.rs
+++ src/macros.rs
@@ -3,8 +3,8 @@ use crate::CStr8;
 
 /// Create a const <code>&'static [CStr8]</code> from a const string.
 ///
-/// The macro accepts these types as input, rejecting them at compile time
-/// if they contain a nul byte or aren't UTF-8:
+/// The macro accepts any of the following string-like types as input,
+/// causing a compile error if they contain a nul byte or aren't UTF-8:
 ///
 /// - `&str`
 /// - `&CStr`
@@ -27,21 +27,17 @@ use crate::CStr8;
 /// assert_eq!(WITH_NUL, "constant");
 /// assert_eq!(WITH_NUL.as_bytes_with_nul(), b"constant\0");
 ///
-/// const CSTR_CONSTANT: &CStr = match CStr::from_bytes_until_nul(b"cstr constant\0") {
-///     Ok(x) => x,
-///     Err(_) => unreachable!(),
-/// };
+/// const CSTR_CONSTANT: &CStr = c"cstr constant";
 /// assert_eq!(cstr8!(CSTR_CONSTANT).as_bytes_with_nul(), b"cstr constant\0");
 ///
 /// const BYTES_CONSTANT: &[u8] = b"bytes constant";
-/// assert_eq!(cstr8!(BYTES_CONSTANT), "bytes constant");
+/// assert_eq!(cstr8!(BYTES_CONSTANT).as_bytes_with_nul(), b"bytes constant\0");
 ///
 /// const BYTE_ARRAY_REF_CONSTANT: &[u8; 23] = b"byte array ref constant";
 /// assert_eq!(cstr8!(BYTE_ARRAY_REF_CONSTANT).as_bytes_with_nul(), b"byte array ref constant\0");
 ///
 /// const BYTE_ARRAY_CONSTANT: [u8; 19] = *b"byte array constant";
 /// assert_eq!(cstr8!(BYTE_ARRAY_CONSTANT).as_bytes_with_nul(), b"byte array constant\0");
-///
 /// ```
 ///
 /// Internal nul bytes will cause a compilation error:
@@ -62,8 +58,8 @@ use crate::CStr8;
 ///
 /// # Restrictions
 ///
-/// The `cstr8!` macro can't access local generic types, even if constant.
-/// This can be fixed when [`inline_const`] stabilizes.
+/// The `cstr8!` macro can't reference any generic types, even if constant.
+/// Once [`inline_const`] stabilizes, this restriction can be lifted.
 ///
 /// ```compile_fail
 /// use cstr8::{cstr8, CStr8};
@@ -81,8 +77,8 @@ use crate::CStr8;
 macro_rules! cstr8 {
     ($s:expr) => {{
         const __CSTR8_ARRAY: $crate::__internal_unstable::CStr8Array<
-            { $crate::__internal_unstable::CStr8Builder($s).len_before_nul() },
-        > = $crate::__internal_unstable::CStr8Builder($s).build_or_const_panic();
+            { $crate::__internal_unstable::CStr8Builder($s).len_until_nul() },
+        > = $crate::__internal_unstable::CStr8Builder($s).into_array();
         __CSTR8_ARRAY.as_cstr8()
     }};
 }
@@ -99,53 +95,53 @@ pub mod __internal_unstable {
 
     pub struct CStr8Builder<T>(pub T);
 
-    impl CStr8Builder<&'static str> {
-        pub const fn len_before_nul(self) -> usize {
+    impl CStr8Builder<&'_ str> {
+        pub const fn len_until_nul(self) -> usize {
             self.0.len()
         }
 
-        pub const fn build_or_const_panic<const N: usize>(self) -> CStr8Array<N> {
+        pub const fn into_array<const N: usize>(self) -> CStr8Array<N> {
             CStr8Array::from_str(self.0)
         }
     }
 
-    impl CStr8Builder<&'static [u8]> {
-        pub const fn len_before_nul(self) -> usize {
+    impl CStr8Builder<&'_ [u8]> {
+        pub const fn len_until_nul(self) -> usize {
             self.0.len()
         }
 
-        pub const fn build_or_const_panic<const N: usize>(self) -> CStr8Array<N> {
-            CStr8Array::from_bytes(self.0)
+        pub const fn into_array<const N: usize>(self) -> CStr8Array<N> {
+            CStr8Array::from_utf8_without_nul(self.0)
         }
     }
 
-    impl<const N: usize> CStr8Builder<&'static [u8; N]> {
-        pub const fn len_before_nul(self) -> usize {
+    impl<const N: usize> CStr8Builder<&'_ [u8; N]> {
+        pub const fn len_until_nul(self) -> usize {
             N
         }
 
-        pub const fn build_or_const_panic(self) -> CStr8Array<N> {
-            CStr8Array::from_bytes(self.0)
+        pub const fn into_array(self) -> CStr8Array<N> {
+            CStr8Array::from_utf8_without_nul(self.0)
         }
     }
 
     impl<const N: usize> CStr8Builder<[u8; N]> {
-        pub const fn len_before_nul(self) -> usize {
+        pub const fn len_until_nul(self) -> usize {
             N
         }
 
-        pub const fn build_or_const_panic(self) -> CStr8Array<N> {
-            CStr8Array::from_bytes(&self.0)
+        pub const fn into_array(self) -> CStr8Array<N> {
+            CStr8Array::from_utf8_without_nul(&self.0)
         }
     }
 
-    impl CStr8Builder<&'static CStr> {
-        pub const fn len_before_nul(self) -> usize {
+    impl CStr8Builder<&'_ CStr> {
+        pub const fn len_until_nul(self) -> usize {
             self.0.to_bytes().len()
         }
 
-        pub const fn build_or_const_panic<const N: usize>(self) -> CStr8Array<N> {
-            CStr8Array::from_bytes(self.0.to_bytes())
+        pub const fn into_array<const N: usize>(self) -> CStr8Array<N> {
+            CStr8Array::from_utf8_without_nul(self.0.to_bytes())
         }
     }
 
@@ -170,18 +166,17 @@ pub mod __internal_unstable {
             Self(out, 0)
         }
 
-        const fn from_bytes(b: &[u8]) -> Self {
-            Self::from_str(match str::from_utf8(b) {
-                Ok(x) => x,
-                Err(_) => panic!("cstr8: input is not valid UTF-8"),
-            })
+        const fn from_utf8_without_nul(b: &[u8]) -> Self {
+            let Ok(s) = str::from_utf8(b) else {
+                panic!("cstr8: input is not valid UTF-8");
+            };
+            Self::from_str(s)
         }
 
         pub const fn as_cstr8(&'static self) -> &'static CStr8 {
             // SAFETY:
-            // - `Self` is #[repr(C)] and has no padding, and therefore
-            //   is initialized for every byte.
-            // - The only `\0` byte is guaranteed by `new` to be the last byte.
+            // - `Self` is #[repr(C)] and has no padding, thus is fully initialized.
+            // - The only nul byte is guaranteed by construction to be the last byte.
             unsafe {
                 CStr8::from_utf8_with_nul_unchecked(slice::from_raw_parts(
                     (self as *const Self).cast(),
@@ -190,71 +185,66 @@ pub mod __internal_unstable {
             }
         }
     }
-
-    /// Doctests to ensure that the `cstr8!` macro compile-fails as expected.
-    /// It must be located in a public module to be checked with `cargo test`.
-    ///
-    /// ```
-    /// use core::ffi::CStr;
-    /// use cstr8::{cstr8, CStr8};
-    /// cstr8!("control test");
-    /// cstr8!(match CStr::from_bytes_until_nul(b"control cstr\0") {
-    ///     Ok(x) => x,
-    ///     Err(_) => unreachable!(),
-    /// });
-    /// ```
-    ///
-    /// ```compile_fail
-    /// use cstr8::{cstr8, CStr8};
-    /// cstr8!("ends in nul \0");
-    /// ```
-    ///
-    /// ```compile_fail
-    /// use cstr8::{cstr8, CStr8};
-    /// cstr8!("str with \0 nul");
-    /// ```
-    ///
-    /// ```compile_fail
-    /// use cstr8::{cstr8, CStr8};
-    /// cstr8!(b"bytes with \0 nul");
-    /// ```
-    ///
-    /// ```compile_fail
-    /// use cstr8::{cstr8, CStr8};
-    /// cstr8!(b"bytes with \xa0\xa1 invalid utf-8");
-    /// ```
-    ///
-    /// ```compile_fail
-    /// use cstr8::{cstr8, CStr8};
-    /// cstr8!(&b"bytes with \0 nul"[..]);
-    /// ```
-    ///
-    /// ```compile_fail
-    /// use cstr8::{cstr8, CStr8};
-    /// cstr8!(&b"bytes with \xa0\xa1 invalid utf-8"[..]);
-    /// ```
-    ///
-    /// ```compile_fail
-    /// use cstr8::{cstr8, CStr8};
-    /// cstr8!(*b"byte array with \0 nul");
-    /// ```
-    ///
-    /// ```compile_fail
-    /// use cstr8::{cstr8, CStr8};
-    /// cstr8!(b"bytes with \xa0\xa1 invalid utf-8");
-    /// ```
-    ///
-    /// ```compile_fail
-    /// use core::ffi::CStr;
-    /// use cstr8::{cstr8, CStr8};
-    /// cstr8!(match CStr::from_bytes_until_nul(b"cstr with \xa0\xa1 invalid utf-8\0") {
-    ///     Ok(x) => x,
-    ///     Err(_) => unreachable!(),
-    /// });
-    /// ```
-    #[allow(dead_code)]
-    pub struct TestMacroCompileFail;
 }
+
+/// Extra compile_fail tests.
+///
+/// ```
+/// use core::ffi::CStr;
+/// use cstr8::{cstr8, CStr8};
+/// cstr8!("control test");
+/// cstr8!(match CStr::from_bytes_until_nul(b"control cstr\0") {
+///     Ok(x) => x,
+///     Err(_) => unreachable!(),
+/// });
+/// ```
+///
+/// ```compile_fail
+/// use cstr8::{cstr8, CStr8};
+/// cstr8!("ends in nul \0");
+/// ```
+///
+/// ```compile_fail
+/// use cstr8::{cstr8, CStr8};
+/// cstr8!("str with \0 nul");
+/// ```
+///
+/// ```compile_fail
+/// use cstr8::{cstr8, CStr8};
+/// cstr8!(b"bytes with \0 nul");
+/// ```
+///
+/// ```compile_fail
+/// use cstr8::{cstr8, CStr8};
+/// cstr8!(b"bytes with \xa0\xa1 invalid utf-8");
+/// ```
+///
+/// ```compile_fail
+/// use cstr8::{cstr8, CStr8};
+/// cstr8!(&b"bytes with \0 nul"[..]);
+/// ```
+///
+/// ```compile_fail
+/// use cstr8::{cstr8, CStr8};
+/// cstr8!(&b"bytes with \xa0\xa1 invalid utf-8"[..]);
+/// ```
+///
+/// ```compile_fail
+/// use cstr8::{cstr8, CStr8};
+/// cstr8!(*b"byte array with \0 nul");
+/// ```
+///
+/// ```compile_fail
+/// use cstr8::{cstr8, CStr8};
+/// cstr8!(b"bytes with \xa0\xa1 invalid utf-8");
+/// ```
+///
+/// ```compile_fail
+/// use core::ffi::CStr;
+/// use cstr8::{cstr8, CStr8};
+/// cstr8!(c"cstr with \xa0\xa1 invalid utf-8");
+/// ```
+const _: () = ();
 
 #[cfg(test)]
 mod tests {
@@ -273,8 +263,6 @@ mod tests {
         const STR_WITH_NUL: &CStr8 = cstr8!(STR_CONSTANT);
         assert_eq!(STR_WITH_NUL, "constant");
         assert_eq!(STR_WITH_NUL.as_bytes_with_nul(), b"constant\0");
-
-        // There is no `&[u8]` literal syntax.
 
         const BYTES_CONSTANT: &[u8] = b"bytes constant";
         const BYTES_WITH_NUL: &CStr8 = cstr8!(BYTES_CONSTANT);
@@ -308,13 +296,11 @@ mod tests {
             b"byte array constant\0"
         );
 
-        // CStr literals cannot yet be tested because the MSRV of this crate is before
-        // the stabilization of c"" literals scheduled for 1.77.
+        const CSTR_LITERAL: &CStr8 = cstr8!(c"cstr literal");
+        assert_eq!(CSTR_LITERAL, "cstr literal");
+        assert_eq!(CSTR_LITERAL.as_bytes_with_nul(), b"cstr literal\0");
 
-        const CSTR_CONSTANT: &CStr = match CStr::from_bytes_until_nul(b"cstr constant\0") {
-            Ok(x) => x,
-            Err(_) => unreachable!(),
-        };
+        const CSTR_CONSTANT: &CStr = c"cstr constant";
         const CSTR_UTF8: &CStr8 = cstr8!(CSTR_CONSTANT);
         assert_eq!(CSTR_UTF8, "cstr constant");
         assert_eq!(CSTR_UTF8.as_bytes_with_nul(), b"cstr constant\0");
